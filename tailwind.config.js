592cb55bb05c163a75b64fd1d077d3199453e305
asgharali101 <asgharburdi786@gmail.com>
2025-03-16 20:59:18 +0500

    design ecommerce store

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html', // For the main HTML file
+    './src/**/*.{html,js,ts,jsx,tsx}', // Include all files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
